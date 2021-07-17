1cc05eae3d8042460cf4e528287b8ea328b0b67a
roshan <kroshan1895@gmail.com>
2021-07-18 01:12:36 +0530

    Update Practice1.kt

--- Practice1.kt
+++ Practice1.kt
@@ -1,10 +1,16 @@
 
 fun main()
 {
- print("(\\  (\\\n( -.- )\nO_(\")(\")");
+ var line1="(\\  (\\"
+ var line2="( -.- )"
+ var line3="O_(\")(\")"
+ println(line1)
+ println(line2)
+ println(line3)
+
  /*
    Output is:-
-   
+
          (\  (\
          ( -.- )
          O_(")(")
